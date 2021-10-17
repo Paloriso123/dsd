cd80c98c8bcb2b54b55f81572f04148956382fab
Paloriso123 <paloriso123@gmail.com>
2021-10-17 21:03:32 +0200

    skratky.php

---
+++ skratkyTipsTricks.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- KLAVESOVE SKRATKY -->
+    <!-- COMMENT                        L-Ctrl + / -->
+    <!-- KOPIROVAT RIADOK               L-SHIFT + L-ALT + ARROW DOWN/UP -->
+    <!-- PRESUNUT RIADOK                L-ALT + ARROW DOWN/UP  -->
+    <!-- VYZNACENE SLOVO NAJST V TEXTE  L-CTRL + D -->
+    <!-- VYTVORIT DIV s class a id                  .classa#idcko -->
+</body>
+</html>
