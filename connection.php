bfcf412e62cc7d9fe5a5e412f99d382cd4e9532c
Paloriso123 <paloriso123@gmail.com>
2021-10-31 21:06:28 +0100

    naplnenie connection.php spravnymi udajmi k dbsam

--- connection.php
+++ connection.php
@@ -5,11 +5,16 @@ error_reporting(E_ALL);
 
 require_once('functions.php');
 
-$conn = connectToDBS('localhost', 'root', 'root', 'c_redakcia');
-$conn2 = connectToDBS('25.46.84.8', 'bondra', 'test21', 'c_redakcia');
+// $conn = connectToDBS('serverIP', 'meno', 'heslo', 'nazovDatabazy');
+$conn1 = connectToDBS('25.60.204.245', 'risoDesktop', 'Palorisodsd', 'finance_blog');
+$conn2 = connectToDBS('25.79.132.0', 'risoNotebook', 'Palorisodsd', 'finance_blog');
+$conn3 = connectToDBS('25.77.19.200', 'paloDesktop', 'Palorisodsd', 'finance_blog');
+$conn4 = connectToDBS('25.79.70.7', 'paloNotebook', 'Palorisodsd', 'finance_blog');
 
 $connection = [
-     'conn' => $conn,
-     'conn2' => $conn2
+     'conn1' => $conn1,
+     'conn2' => $conn2,
+     'conn3' => $conn3,
+     'conn4' => $conn4
  ];      
 
