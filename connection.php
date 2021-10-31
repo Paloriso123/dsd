80fbffa7fa357a12f6098eda84feea8ae76b0c1e
Paloriso123 <paloriso123@gmail.com>
2021-10-31 22:52:07 +0100

    connection succeded

--- connection.php
+++ connection.php
@@ -6,15 +6,20 @@ error_reporting(E_ALL);
 require_once('functions.php');
 
 // $conn = connectToDBS('serverIP', 'meno', 'heslo', 'nazovDatabazy');
-$conn = connectToDBS('25.60.204.245', 'risoDesktop', 'Palorisodsd', 'finance_blog');
-$conn2 = connectToDBS('25.79.132.0', 'risoNotebook', 'Palorisodsd', 'finance_blog');
-$conn3 = connectToDBS('25.77.19.200', 'paloDesktop', 'Palorisodsd', 'finance_blog');
-$conn4 = connectToDBS('25.79.70.7', 'paloNotebook', 'Palorisodsd', 'finance_blog');
+$conn = connectToDBS('25.60.204.245', 'paloDesktop', 'Palorisodsd', 'finance_blog');
+$conn1 = connectToDBS('25.77.19.200', 'paloDesktop', 'Palorisodsd', 'finance_blog');
+$conn2 = connectToDBS('25.79.70.7', 'paloDesktop', 'Palorisodsd', 'finance_blog');
+$conn3 = connectToDBS('25.79.132.0', 'paloDesktop', 'Palorisodsd', 'finance_blog');
+
+
+
+
+
 
 $connection = [
      'conn' => $conn,
+     'conn1' => $conn1,
      'conn2' => $conn2,
-     'conn3' => $conn3,
-     'conn4' => $conn4
- ];      
+     'conn3' => $conn3
+];      
 
