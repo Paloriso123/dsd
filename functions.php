12210aea521a54a781e87b8df5a17e85c2e4d7ff
Paloriso123 <paloriso123@gmail.com>
2021-10-31 23:02:24 +0100

    upravene po

--- functions.php
+++ functions.php
@@ -13,6 +13,8 @@ function connectToDBS($servername, $username, $password, $dbname) {
         PDO::ATTR_TIMEOUT => 1, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
         echo "Connection succeded on  $servername <br>";
 
+        // risoDesktop 25.60.204.245, nefunguje mysql/db a tam nejsu udaje nikoho z pouzivatelov ... natvrdo sme to nakopirovali
+
         return $conn;
     } catch (PDOException $e) {
         echo "Connection failed $servername :  " . $e->getMessage() . " <br>";
